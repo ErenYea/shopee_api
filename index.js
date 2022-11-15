5212908f9aa9bc1868684f607ae57b9674e965d9
Hamza Ali Khan <drstoneeren@gmail.com>
2022-11-15 19:44:55 +0500

    changes

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-import app from "./server.js"
-import mongodb from "mongodb"
-import dotenv from "dotenv"
-import ProductsDAO from "./dao/productsDAO.js"
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import ProductsDAO from "./dao/productsDAO.js";
+import express from "express";
+import cors from "cors";
+import products from "./api/products.route.js";
 
-dotenv.config()
+dotenv.config();
 
-const MongoClient = mongodb.MongoClient
+const MongoClient = mongodb.MongoClient;
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-MongoClient.connect(
-    process.env.RESTREVIEWS_DB_URI,
-    {
-        maxPoolSize: 50,
-        wtimeoutMS: 2500,
-        useNewUrlParser: true
-    })
-    .catch( err => {
-        console.error(err.stack)
-        process.exit(1)
-    })
-    .then( async client => {
+const app = express();
 
-        await ProductsDAO.injectDB(client)
+app.use(cors());
+app.use(express.json());
 
-        app.listen(port, () => {
-            console.log(`listening on port ${port}`)
-        })
-    }
-)
+app.use("/api/v1/products", products);
+
+app.use("*", (req, res) => res.status(404).json({ error: "Not Found" }));
+
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
+  maxPoolSize: 50,
+  wtimeoutMS: 2500,
+  useNewUrlParser: true,
+})
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    await ProductsDAO.injectDB(client);
+
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  });
+
+export default app;
