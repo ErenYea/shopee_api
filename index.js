5e56e7ed5896b7ff6b7cb35753c68878d93ad533
Hamza Ali Khan <drstoneeren@gmail.com>
2022-11-15 20:32:27 +0500

    aa

--- index.js
+++ index.js
@@ -16,7 +16,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/products", products);
+app.use("/api/v1/products", async (req, res) => {
+  const limit = req.query.limit ? parseInt(req.query.limit, 10) : 100;
+
+  const page = req.query.page ? parseInt(req.query.page, 10) : 0;
+
+  let filters = {};
+  if (req.query.dest) {
+    filters.dest = req.query.dest;
+  } else if (req.query.name) {
+    filters.name = req.query.name;
+  } else if (req.query.brand) {
+    filters.brand = req.query.brand;
+  }
+
+  const { productsList, totalNumProducts } = await ProductsDAO.getProducts({
+    filters,
+    page,
+    limit,
+  });
+
+  let response = {
+    products: productsList,
+    page: page,
+    filters: filters,
+    entries_per_page: limit,
+    total_results: totalNumProducts,
+  };
+  res.json(response);
+});
 
 app.use("*", (req, res) => res.status(404).json({ error: "Not Found" }));
 
