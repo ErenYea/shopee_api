4c1689344116e3f6382c68312e296c73c4c69da7
Hamza Ali Khan <drstoneeren@gmail.com>
2022-11-15 21:19:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,50 +4,9 @@ const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 
-dotenv.config();
+// dotenv.config();
 
 const MongoClient = mongodb.MongoClient;
-let products;
-// class ProductsDAO {
-//   static async injectDB(conn) {}
-
-//   static async getProducts({ filters = null, page = 0, limit = 100 } = {}) {
-//     let query;
-//     if (filters) {
-//       if ("brand" in filters) {
-//         query = { $text: { $search: filters["brand"] } };
-//       } else if ("dest" in filters) {
-//         query = { $text: { $search: `|${filters["dest"]}|` } };
-//       } else if ("name" in filters) {
-//         query = { $text: { $search: filters["name"] } };
-//       }
-//     }
-
-//     let cursor;
-//     try {
-//       console.log("query", query);
-//       cursor = await products.find(query);
-//       console.log("cursor found");
-//     } catch (e) {
-//       console.error(`Unable to issue find command, ${e}`);
-//       return { productsList: [], totalNumProducts: 0 };
-//     }
-
-//     const displayCursor = cursor.limit(limit).skip(limit * page);
-
-//     try {
-//       const productsList = await displayCursor.toArray();
-//       const totalNumProducts = await products.countDocuments(query);
-
-//       return { productsList, totalNumProducts };
-//     } catch (e) {
-//       console.error(
-//         `Unable to convert cursor to array or problem counting documents, ${e}`
-//       );
-//       return { productsList: [], totalNumProducts: 0 };
-//     }
-//   }
-// }
 
 const port = process.env.PORT || 8000;
 
