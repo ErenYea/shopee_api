bbd1b0b6ce24f3eea4f9e6de876c573ea435201c
Hamza Ali Khan <drstoneeren@gmail.com>
2022-11-15 21:09:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,46 +8,46 @@ dotenv.config();
 
 const MongoClient = mongodb.MongoClient;
 let products;
-class ProductsDAO {
-  static async injectDB(conn) {}
+// class ProductsDAO {
+//   static async injectDB(conn) {}
 
-  static async getProducts({ filters = null, page = 0, limit = 100 } = {}) {
-    let query;
-    if (filters) {
-      if ("brand" in filters) {
-        query = { $text: { $search: filters["brand"] } };
-      } else if ("dest" in filters) {
-        query = { $text: { $search: `|${filters["dest"]}|` } };
-      } else if ("name" in filters) {
-        query = { $text: { $search: filters["name"] } };
-      }
-    }
+//   static async getProducts({ filters = null, page = 0, limit = 100 } = {}) {
+//     let query;
+//     if (filters) {
+//       if ("brand" in filters) {
+//         query = { $text: { $search: filters["brand"] } };
+//       } else if ("dest" in filters) {
+//         query = { $text: { $search: `|${filters["dest"]}|` } };
+//       } else if ("name" in filters) {
+//         query = { $text: { $search: filters["name"] } };
+//       }
+//     }
 
-    let cursor;
-    try {
-      console.log("query", query);
-      cursor = await products.find(query);
-      console.log("cursor found");
-    } catch (e) {
-      console.error(`Unable to issue find command, ${e}`);
-      return { productsList: [], totalNumProducts: 0 };
-    }
+//     let cursor;
+//     try {
+//       console.log("query", query);
+//       cursor = await products.find(query);
+//       console.log("cursor found");
+//     } catch (e) {
+//       console.error(`Unable to issue find command, ${e}`);
+//       return { productsList: [], totalNumProducts: 0 };
+//     }
 
-    const displayCursor = cursor.limit(limit).skip(limit * page);
+//     const displayCursor = cursor.limit(limit).skip(limit * page);
 
-    try {
-      const productsList = await displayCursor.toArray();
-      const totalNumProducts = await products.countDocuments(query);
+//     try {
+//       const productsList = await displayCursor.toArray();
+//       const totalNumProducts = await products.countDocuments(query);
 
-      return { productsList, totalNumProducts };
-    } catch (e) {
-      console.error(
-        `Unable to convert cursor to array or problem counting documents, ${e}`
-      );
-      return { productsList: [], totalNumProducts: 0 };
-    }
-  }
-}
+//       return { productsList, totalNumProducts };
+//     } catch (e) {
+//       console.error(
+//         `Unable to convert cursor to array or problem counting documents, ${e}`
+//       );
+//       return { productsList: [], totalNumProducts: 0 };
+//     }
+//   }
+// }
 
 const port = process.env.PORT || 8000;
 
